2d06a66fdb4bd5acb80fb0d9065a740c93d88e95
vladislav <llpvo2602ll@gmail.com>
2023-10-21 10:36:15 +0300

    create django project

---
+++ core/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+from drf_spectacular import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
+    path('api/schema/', views.SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs', views.SpectacularSwaggerView.as_view(), name='docs'),
+]
