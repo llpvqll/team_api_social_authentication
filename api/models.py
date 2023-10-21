36c45063c47b29fa99f083cd9a14ea9583176a9e
vladislav <llpvo2602ll@gmail.com>
2023-10-21 10:51:38 +0300

    add team model, and update base user model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    teams = models.ManyToManyField('Team', related_name='team_members', blank=True)
+    groups = models.ManyToManyField('auth.Group', related_name='custom_user_set')
+    user_permissions = models.ManyToManyField('auth.Permission', related_name='custom_user_set')
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+
+class Team(models.Model):
+    team_name = models.CharField(max_length=255, unique=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    description = models.TextField()
+    members = models.ManyToManyField(User, related_name='user_teams', blank=True)
