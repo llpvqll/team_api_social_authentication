c87708a8cb790cd6d2a47aa3293d2d7aaeda17a2
vladislav <llpvo2602ll@gmail.com>
2023-10-23 10:55:35 +0300

    add tests for user model

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,51 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+
+def create_user(email='user@example.com', password='testpass123', first_name='admin', last_name='admin'):
+    return get_user_model().objects.create_user(
+        email=email, first_name=first_name, last_name=last_name, password=password
+    )
+
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_successful(self):
+        email = 'test@example.com'
+        password = 'testpass123'
+        first_name = 'test_name'
+        last_name = 'test_surname'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password,
+            first_name=first_name,
+            last_name=last_name
+        )
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        sample_emails = [
+            ['test1@EXAMPLE.com', 'test1@example.com'],
+            ['Test2@Example.com', 'Test2@example.com'],
+            ['TEST3@EXAMPLE.com', 'TEST3@example.com'],
+            ['test4@example.COM', 'test4@example.com'],
+        ]
+        for email, expected in sample_emails:
+            user = get_user_model().objects.create_user(email, 'test_name', 'test_surname', 'sample123')
+            self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        with self.assertRaises(TypeError):
+            get_user_model().objects.create_user('', 'test123')
+
+    def test_create_superuser(self):
+        user = get_user_model().objects.create_superuser(
+            'test@example.com',
+            'test123',
+            'test_name',
+            'test_surname',
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
